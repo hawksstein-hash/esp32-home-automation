02927eb156f80398d1bcd2c5147335513654fe70
hawksstein-hash <riddhisaurana@gmail.com>
2026-09-13 14:53:46 +0530

    esp and relay responsing to webpage

--- esp32/home_automation/home_automation.ino
+++ esp32/home_automation/home_automation.ino
@@ -5,13 +5,80 @@ const char* ssid = "GBPIET_WiFi";
 
 WebServer server(80);
 
+const int LIGHT1_PIN = 2;
+const int RELAY_PIN = 23;
+
+
+// Add CORS permission to every response
+void addCORS() {
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.sendHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  server.sendHeader("Access-Control-Allow-Headers", "Content-Type");
+}
+
+
+// Home page
 void handleRoot() {
+  addCORS();
   server.send(200, "text/plain", "ESP32 Home Automation is working!");
 }
 
+
+// Light 1 ON
+void handleLight1On() {
+  digitalWrite(LIGHT1_PIN, HIGH);
+
+  addCORS();
+  server.send(200, "text/plain", "Light 1 is ON");
+}
+
+
+// Light 1 OFF
+void handleLight1Off() {
+  digitalWrite(LIGHT1_PIN, LOW);
+
+  addCORS();
+  server.send(200, "text/plain", "Light 1 is OFF");
+}
+
+
+// Relay ON
+void handleRelayOn() {
+  digitalWrite(RELAY_PIN, LOW);
+
+  addCORS();
+  server.send(200, "text/plain", "Relay is ON");
+}
+
+
+// Relay OFF
+void handleRelayOff() {
+  digitalWrite(RELAY_PIN, HIGH);
+
+  addCORS();
+  server.send(200, "text/plain", "Relay is OFF");
+}
+
+
+// Handle browser preflight request
+void handleOptions() {
+  addCORS();
+  server.send(204);
+}
+
+
 void setup() {
+
   Serial.begin(115200);
-  delay(1000);
+
+  pinMode(LIGHT1_PIN, OUTPUT);
+  digitalWrite(LIGHT1_PIN, LOW);
+
+  pinMode(RELAY_PIN, OUTPUT);
+
+  // Relay OFF initially
+  digitalWrite(RELAY_PIN, HIGH);
+
 
   Serial.println();
   Serial.println("Connecting to Wi-Fi...");
@@ -29,11 +96,28 @@ void setup() {
   Serial.print("ESP32 IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", handleRoot);
+
+  // Routes
+  server.on("/", HTTP_GET, handleRoot);
+
+  server.on("/light1/on", HTTP_GET, handleLight1On);
+  server.on("/light1/off", HTTP_GET, handleLight1Off);
+
+  server.on("/relay/on", HTTP_GET, handleRelayOn);
+  server.on("/relay/off", HTTP_GET, handleRelayOff);
+
+  server.on("/light1/on", HTTP_OPTIONS, handleOptions);
+  server.on("/light1/off", HTTP_OPTIONS, handleOptions);
+
+  server.on("/relay/on", HTTP_OPTIONS, handleOptions);
+  server.on("/relay/off", HTTP_OPTIONS, handleOptions);
+
 
   server.begin();
+
   Serial.println("Web server started!");
 }
+
 
 void loop() {
   server.handleClient();
--- web/script.js
+++ web/script.js
@@ -8,39 +8,97 @@ const devices = {
 };
 
 
-function controlDevice(device, action) {
+// ESP32 IP address
+const ESP32_IP = "192.168.15.128";
+
+
+// Map website devices to ESP32 routes
+const deviceRoutes = {
+
+    light1: "light1",
+    light2: "relay"
+
+};
+
+
+async function controlDevice(device, action) {
 
     const status =
         document.getElementById(device + "-status");
 
+    try {
 
-    if (action === "on") {
+        // Check whether this device is connected to ESP32
+        if (!deviceRoutes[device]) {
 
-        devices[device] = true;
+            alert(device + " is not connected to ESP32 yet.");
 
-        status.textContent = "ON";
+            return;
 
-        status.classList.add("status-on");
+        }
 
-        status.classList.remove("status-off");
+
+        // Get the correct ESP32 route
+        const espDevice = deviceRoutes[device];
+
+
+        // Send command to ESP32
+        const response = await fetch(
+            `http://${ESP32_IP}/${espDevice}/${action}`
+        );
+
+
+        if (!response.ok) {
+
+            throw new Error("ESP32 did not respond");
+
+        }
+
+
+        // Get response from ESP32
+        const message = await response.text();
+
+        console.log("ESP32:", message);
+
+
+        // Update webpage after ESP32 responds
+        if (action === "on") {
+
+            devices[device] = true;
+
+            status.textContent = "ON";
+
+            status.classList.add("status-on");
+
+            status.classList.remove("status-off");
+
+        }
+
+        else {
+
+            devices[device] = false;
+
+            status.textContent = "OFF";
+
+            status.classList.add("status-off");
+
+            status.classList.remove("status-on");
+
+        }
+
+
+        updateDeviceCount();
 
     }
 
 
-    else {
+    catch (error) {
 
-        devices[device] = false;
+        console.error("ESP32 connection failed:", error);
 
-        status.textContent = "OFF";
-
-        status.classList.add("status-off");
-
-        status.classList.remove("status-on");
+        alert("Could not connect to ESP32.");
 
     }
-
-
-    updateDeviceCount();
 
 }
 
@@ -49,7 +107,12 @@ function controlAll(action) {
 
     for (let device in devices) {
 
-        controlDevice(device, action);
+        // Only control devices that are actually connected
+        if (deviceRoutes[device]) {
+
+            controlDevice(device, action);
+
+        }
 
     }
 
